7d57c230742ea78430fa92d8c885a6ca0f837d73
Zack BATIK <zbatik@e4r21p2.wethinkcode.co.za>
2018-12-17 13:56:16 +0200

    photo booth done

--- booth.php
+++ booth.php
@@ -1,3 +1,8 @@
 <?php require $_SERVER["DOCUMENT_ROOT"]."/camagru/shared/header.php"; ?>
 
+<video style=""id="video" width="400" height="300" src=""></video>
+<canvas id="canvas" width="400" height="300"> </canvas>
+<img alt="" id="upload_2" style="width : 400px; position : absolute; z-index: -20;" hidden="true"src="">
+<button id="capture-but">take photo</button>
+<div id="photo-reel"></div>
 <?php require $_SERVER["DOCUMENT_ROOT"]."/camagru/shared/footer.php"; ?>
--- js/camagru.js
+++ js/camagru.js
@@ -53,3 +53,37 @@ function PostUpdateForm(form_id, post_to) {
         }
     });
 }
+
+window.addEventListener("DOMContentLoaded",function() {
+
+    var video = document.getElementById("video"),
+       canvas = document.getElementById("canvas"),
+       context = canvas.getContext("2d"),
+
+       vendorUrl = window.URL || window.webkitURL;
+
+   navigator.getMedia =    navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetusermedia ||
+                           navigator.msGetUserMedia;
+
+   navigator.getMedia({
+       video: true,
+       audio: false
+   }, function(stream) {
+       video.srcObject = stream;
+       video.play();
+   }, function(error) {
+       has_webcam = false;
+      // cap_btn.hidden = true;
+   });
+    document.getElementById("capture-but").addEventListener('click', function(){
+        context.drawImage(video, 0, 0, 400, 300);
+        currentpic = canvas.toDataURL();
+        photo = document.createElement('img');
+        photo.setAttribute("src", currentpic);
+        camera_roll = document.getElementById("photo-reel");
+        camera_roll.insertBefore(photo, camera_roll.firstElementChild);
+        photo.setAttribute("style", "transform: rotateY(180deg);-webkit-transform:rotateY(180deg); -moz-transform:rotateY(180deg); ");
+    });
+});
